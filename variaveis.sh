6437c839d9159967cbdddd7d1d1d7769f8b86cc8
Bryant-Anjos <bryant_mx@hotmail.com>
2019-05-21 23:41:53 -0300

    Aulas de variáveis OK

--- variaveis.sh
+++ variaveis.sh
@@ -1,3 +1,31 @@
 #!/usr/bin/env bash
 
-echo "Teste"
+NOME="Bryant dos Anjos"
+
+echo "$NOME"
+
+NUMERO_1=23
+NUMERO_2=45
+
+TOTAL=$(($NUMERO_1+$NUMERO_2))
+
+echo "$TOTAL"
+
+SAIDA_CAT=$(cat /etc/passwd | grep brilliant)
+
+echo "$SAIDA_CAT"
+
+echo "-------------------------------------------------"
+
+echo "Parâmetro 1: $1"
+echo "Parâmetro 2: $2"
+
+echo "Todos os parâmetros: $*"
+
+echo "Quantos parâmetros? $#"
+
+echo "Saída do último comando: $?"
+
+echo "PID: $$"
+
+echo "$0"
