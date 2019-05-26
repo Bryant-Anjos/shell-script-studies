1d7da9210a5f1846a60e3787b447899593d4d9fa
Bryant-Anjos <bryant_mx@hotmail.com>
2019-05-25 21:44:29 -0300

    Atualizado cripto para validar strings

--- meu_programa/cripto.sh
+++ meu_programa/cripto.sh
@@ -15,7 +15,9 @@
 # Histórico:
 #
 # v0.1 25/05/2019, Bryant:
-#     - O que mudou?
+#     - Criado a função de Salvar strings em HASH em arquivo
+# v0.2 25/05/2019, Bryant:
+#     - Adicionado a validação de textos em HASH
 # ------------------------------------------------------------------------- #
 # Testado em:
 #   bash 4.4.19
@@ -29,13 +31,18 @@
 HELP="
   $(basename $0) - [OPÇÔES]
 
-  -h - Menu de ajuda
-  -v - Versão
-  -a - Define o usuário como anônimo
+  -h - Menu de ajuda.
+  -v - Versão.
+  -c - Criptografa uma string e salva em um arquivo.
+          Usabilidade: -c <TEXTO> <NOME DO ARQUIVO>
+  -d - Verifica se a String está salva em um determinado arquivo
+          Usabilidade: -d <TEXTO> <ARQUIVO>
+  -a - Define o usuário como anônimo. Colocar antes de -c.
 "
-VERSAO="v0.1"
+VERSAO="v0.2"
 HASH=""
 USER="$(whoami)"
+
 # ------------------------------------------------------------------------- #
 
 # -------------------------- TESTES --------------------------------------- #
@@ -57,6 +64,15 @@ Criptografa () {
   echo "$HASH" >> $2
 }
 
+Valida () {
+  local STRING_VERIFICA=$(echo -n $1 | sha256sum | cut -d " " -f 1)
+  local HASH_LIST=$(cat $2 | cut -d ";" -f 1)
+  for HASH_VERIFICA in $HASH_LIST; do
+    [ "$STRING_VERIFICA" = "$HASH_VERIFICA" ] && echo "Verdadeiro" && exit 0
+  done
+  echo "Falso"
+}
+
 # ------------------------------------------------------------------------- #
 
 # -------------------------- EXECUÇÃO ------------------------------------- #
@@ -68,6 +84,7 @@ do
     -v) echo "$VERSAO" && exit 0                                   ;;
     -c) Criptografa $2 $3                                          ;;
     -a) USER="anonymous"                                           ;;
+    -d) Valida $2 $3                                               ;;
 #    *) echo "Comando inválido. Veja as opções disponíveis com -h" ;;
   esac
   shift
