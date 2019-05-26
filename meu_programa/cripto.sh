df796e93904f71f790e290b4938535f95dc74b1c
Bryant-Anjos <bryant_mx@hotmail.com>
2019-05-26 12:23:53 -0300

    Atualizado cripto para a versão 0.3

--- meu_programa/cripto.sh
+++ meu_programa/cripto.sh
@@ -9,8 +9,13 @@
 # explicando a forma de utilizar.
 #
 # Exemplos:
-#     $ ./nomeDoScript.sh -d 1
-#     Neste exemplo o script será executado no modo debug nível 1.
+#     $ ./cripto.sh -c "seu texto aqui" "exemplo" 
+#       Neste exemplo o texto "seu texto aqui" será armazenado na forma do
+#       sha256 no arquivo exemplo.
+#
+#     $ ./cripto.sh -v "seu texto aqui" "exemplo"
+#       Neste exemplo o programa irá verificar se o texto "seu texto aqui"
+#       está dentro do arquivo exemplo.
 # ------------------------------------------------------------------------- #
 # Histórico:
 #
@@ -18,6 +23,9 @@
 #     - Criado a função de Salvar strings em HASH em arquivo
 # v0.2 25/05/2019, Bryant:
 #     - Adicionado a validação de textos em HASH
+# v0.3 26/05/2019, Bryant:
+#     - Adicionado a possibilidade de adicionar comentários e algumas outras
+#       modificações na estrutura do programa.
 # ------------------------------------------------------------------------- #
 # Testado em:
 #   bash 4.4.19
@@ -25,6 +33,14 @@
 # Agradecimentos:
 #
 #   Mateus Müler - Por ensinar os conhecimentos aplicados nesse programa.
+# ------------------------------------------------------------------------- #
+# TODO
+#
+#   Adicionar opção de verificar o usuário caso a validação retorne True.
+#   Adicionar opção de verificar a data caso a validação retorne True.
+#   Adicionar opção de verificar o horário caso a validação retorne True.
+#   Adicionar opção de verificar o comentário caso a validação retorne True.
+#   Adicionar opção de remover um registro caso a validação retorne True.
 # ------------------------------------------------------------------------- #
 
 # -------------------------- VARIÁVEIS ------------------------------------ #
@@ -38,10 +54,15 @@ HELP="
   -d - Verifica se a String está salva em um determinado arquivo
           Usabilidade: -d <TEXTO> <ARQUIVO>
   -a - Define o usuário como anônimo. Colocar antes de -c.
+  -m - Adiciona um comentário.
+          Usabilidade: -m <COMENTARIO>
 "
-VERSAO="v0.2"
+VERSAO="v0.3"
 HASH=""
 USER="$(whoami)"
+CHAVE_CRIPTOGRAFA=0
+CHAVE_VALIDA=0
+COMENTARIO=""
 
 # ------------------------------------------------------------------------- #
 
@@ -60,7 +81,7 @@ Criptografa () {
     echo "Um ou mais argumentos faltando. Veja -h para obter ajuda" && exit 0
   fi
 
-  HASH="$(echo -n $1 | sha256sum | cut -d " " -f 1);$(Data);$USER"
+  HASH="$(echo -n $1 | sha256sum | cut -d " " -f 1);$(Data);$USER$COMENTARIO"
   echo "$HASH" >> $2
 }
 
@@ -82,13 +103,25 @@ do
   case "$1" in
     -h) echo "$HELP" && exit 0                                     ;;
     -v) echo "$VERSAO" && exit 0                                   ;;
-    -c) Criptografa $2 $3                                          ;;
+    -c) CHAVE_CRIPTOGRAFA=1
+        TEXTO="$2"
+        shift
+        NOME_ARQUIVO="$2"
+        shift                                                      ;;
     -a) USER="anonymous"                                           ;;
-    -d) Valida $2 $3                                               ;;
-#    *) echo "Comando inválido. Veja as opções disponíveis com -h" ;;
+    -d) CHAVE_VALIDA=1
+        TEXTO="$2"
+        shift
+        NOME_ARQUIVO="$2"
+        shift                                                      ;;
+    -m) [ $2 ] && COMENTARIO=";$2" && shift                        ;;
+     *) echo "Comando inválido. Veja as opções disponíveis com -h" ;;
   esac
   shift
 done
+
+[ $CHAVE_CRIPTOGRAFA -eq 1 ] && Criptografa $TEXTO $NOME_ARQUIVO
+[ $CHAVE_VALIDA -eq 1 ] && Valida $TEXTO $NOME_ARQUIVO
 
 # ------------------------------------------------------------------------- #
 
