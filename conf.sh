7bb1019b3bb7c8bbcdd6e66c18fee2dd879de82d
Bryant-Anjos <bryant_mx@hotmail.com>
2019-05-29 20:13:00 -0300

    Criando arquivo de configuração

---
+++ conf.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+ARQUIVO_DE_CONFIGURACAO="configuracao.cf"
+USAR_MAIUSCULAS=
+USAR_CORES=
+MENSAGEM="Mensagem de teste"
+
+VERDE="\033[32;1m"
+VERMELHO="033\[31;1n"
+
+[ ! -r "$ARQUIVO_DE_CONFIGURACAO" ] && echo "Não temos acesso de leitura" && exit 1
+
+DefinirParametros () {
+  local parametro="$(echo $1 | cut -d = -f 1)"
+  local valor="$(echo $1 | cut -d = -f 2)"
+
+  case "$parametro" in
+    USAR_CORES)      USAR_CORES=$valor      ;;
+    USAR_MAIUSCULAS) USAR_MAIUSCULAS=$valor ;;
+  esac
+}
+
+while read -r linha
+do
+  [ "$(echo $linha | cut -c1)" = "#" ] && continue
+  [ ! "$linha" ] && continue
+  DefinirParametros "$linha"
+done < "$ARQUIVO_DE_CONFIGURACAO"
+
+[ $USAR_MAIUSCULAS -eq 1 ] && MENSAGEM="$(echo -e $MENSAGEM | tr [a-z] [A-Z])"
+[ $USAR_CORES -eq 1 ] && MENSAGEM="$(echo -e ${VERDE}$MENSAGEM)"
+
+echo "$MENSAGEM"
